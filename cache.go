f6eaae9ddb93ca951ad8b4e46af9e4a14c7fa309
Tuấn Vương <vanhtuan0409@gmail.com>
2018-05-10 16:36:04 +0700

    add godoc

--- cache.go
+++ cache.go
@@ -1,3 +1,4 @@
+// Package cache provides a dead simple implementation of in-mem cache with LRU or LFU eviction method
 package cache
 
 import (
@@ -7,9 +8,11 @@ import (
 )
 
 var (
-	KeyNotFound = errors.New("cache key not found")
+	// ErrKeyNotFound Error when access an unknowned key in cache
+	ErrKeyNotFound = errors.New("Key not found")
 )
 
+// Cache Most simplest interface for cache with 2 method Get and Set
 type Cache interface {
 	Set(key string, value interface{})
 	Get(key string) (interface{}, error)
@@ -23,6 +26,7 @@ type cache struct {
 	items map[string]*list.Element
 }
 
+// NewLRUCache Create new cache with LRU eviction
 func NewLRUCache(size int) Cache {
 	return &cache{
 		size:  size,
@@ -32,6 +36,7 @@ func NewLRUCache(size int) Cache {
 	}
 }
 
+// NewLFUCache Create new cache with LFU eviction
 func NewLFUCache(size int) Cache {
 	return &cache{
 		size:  size,
@@ -41,6 +46,7 @@ func NewLFUCache(size int) Cache {
 	}
 }
 
+// Set Set a new cache value, will trigger eviction if cache is full
 func (c *cache) Set(key string, value interface{}) {
 	c.Lock()
 	defer c.Unlock()
@@ -61,10 +67,11 @@ func (c *cache) Set(key string, value interface{}) {
 	c.items[key] = ele
 }
 
+// Get Get a value from cache by key, return error if key is not in cache
 func (c *cache) Get(key string) (interface{}, error) {
 	ele, ok := c.items[key]
 	if !ok {
-		return nil, KeyNotFound
+		return nil, ErrKeyNotFound
 	}
 
 	c.Lock()
